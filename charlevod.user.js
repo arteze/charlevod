7e135909a08210f38185ad5d8b84cb7703a82bfe
ArtEze <passymas@gmail.com>
2025-01-13 16:07:09 -0300

    Update charlevod.user.js

--- charlevod.user.js
+++ charlevod.user.js
@@ -28,6 +28,22 @@ window.i = {
 			)
 		}
 	}
+	, create_limit_input: function(){
+		var menubar = document.querySelector("#menubar")
+		if(menubar){
+			var a = document.createElement("a")
+			var input = document.createElement("input")
+			input.id = "limit_messages"
+			if(window.localStorage.i){
+				window.i.count_messages = JSON.parse(window.localStorage.i).count_messages
+			}else{
+				window.i.count_messages = 800
+			}
+			input.value = window.i.count_messages
+			a.appendChild(input)
+			document.querySelector("#menubar").appendChild(a)
+		}
+	}
 	, program: function program(){
 		document.onreadystatechange = (function ready(){
 			++window.i.i
@@ -35,7 +51,7 @@ window.i = {
 				// Parte 1
 				window.ol = function ol(a, b, c, d, e) {
 					// console.trace("ol",{ a: a, b: b, c: c, d: d, e: e })
-					e = { limit: 900 };
+					e = { limit: window.i.count_messages };
 					null != d && (e.toTime = d);
 					"room" == b ? e.roomId = c : "private" == b && (e.nick = c);
 					var devuelve = window.jh(a.qa, "loadLastMessages", e, !0)
@@ -44,19 +60,26 @@ window.i = {
 
 				// Parte 2
 				window.i.restart_scroll();
+				
+				// Parte 3: Entrada para el límite
+				window.i.create_limit_input()
 			}
 		})
 		setInterval( function interval(){
+			if( window.limit_messages && window.limit_messages.value>20 ){
+				var ls = {count_messages: window.i.count_messages}
+				window.i.count_messages = +window.limit_messages.value
+				window.localStorage.i = JSON.stringify(ls)
+			}
 			var container = document.querySelector(".chatMessages")
 			if(container){
 				var count = container = document.querySelector(".chatMessages").childNodes.length
 				window.i.counts.push(count)
-				if( window.i.counts[window.i.counts.length-1] > window.i.counts[window.i.counts.length-2] + 800 ){
-					console.log("Subida detectada")
+				if( window.i.counts[window.i.counts.length-1] >
+					window.i.counts[window.i.counts.length-2] + window.i.count_messages - 15 ){
 					var container_scroll = document.querySelector(".chatMessagesContainer")
 					if(container_scroll.scrollTo){
                         container_scroll = document.querySelector(".chatMessagesContainer")
-                        console.log("timeout container_scroll.scrollTo",container_scroll.scrollTo)
                         container_scroll.scrollTo(0, 100); // x: 0, y: 100
 					}
 				}
