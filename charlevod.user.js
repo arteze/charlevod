4936b0e56fa30d3d68eee81671082c0fab2c9fef
ArtEze <passymas@gmail.com>
2025-01-13 01:30:29 -0300

    Update charlevod.user.js

--- charlevod.user.js
+++ charlevod.user.js
@@ -9,22 +9,24 @@
 // ==/UserScript==
 
 window.i = {
-    i: 0
+	i: 0
 }
 document.onreadystatechange = (function(){
-    ++window.i.i
-    if(window.i.i==2){
-        // Parte 0
+	++window.i.i
+	if(window.i.i==2){
+		// Parte 0
 		var chatMessagesWrapper = document.querySelector(".chatMessagesWrapper")
-        chatMessagesWrapper && (chatMessagesWrapper.style["overflow-y"] = "scroll")
+		chatMessagesWrapper && (chatMessagesWrapper.style["overflow-y"] = "scroll")
 
-        // Parte 1
-        window.ol = function ol(a, b, c, d, e) {
-            // console.trace("ol",{ a: a, b: b, c: c, d: d, e: e })
-            e = { limit: 900 };
-            null != d && (e.toTime = d);
-            "room" == b ? e.roomId = c : "private" == b && (e.nick = c);
-            return window.jh(a.qa, "loadLastMessages", e, !0)
-        }
-    }
+		// Parte 1
+		window.ol = function ol(a, b, c, d, e) {
+			// console.trace("ol",{ a: a, b: b, c: c, d: d, e: e })
+			e = { limit: 900 };
+			null != d && (e.toTime = d);
+			"room" == b ? e.roomId = c : "private" == b && (e.nick = c);
+			var devuelve = window.jh(a.qa, "loadLastMessages", e, !0)
+			chatMessagesWrapper && chatMessagesWrapper.scrollTo(0, 100);
+			return devuelve
+		}
+	}
 })
